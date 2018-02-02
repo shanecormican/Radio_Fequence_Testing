f484d99c05beea276da3cb396194bcc20cd1b98c
shanecormican <32567221+shanecormican@users.noreply.github.com>
2018-02-02 16:30:21 +0000

    Add files via upload

--- Recever.ino
+++ Recever.ino
@@ -3,17 +3,42 @@
 
 RH_ASK driver;
 
+const int buttonPin = 2;     // the number of the pushbutton pin
+
+int buttonState = 0;         // variable for reading the pushbutton status
+
 void setup()
 {
-    Serial.begin(9600);   // Debugging only
+    Serial.begin(115200);   // Debugging only
     if (!driver.init())
          Serial.println("init failed");
+
+         
+    pinMode(buttonPin, INPUT);
 }
 
 void loop()
 {
-    const char *msg = "Hello World!";
+   int count = 0;
+  buttonState = digitalRead(buttonPin);
+
+   if (buttonState == HIGH && count == 0) {
+    count++;
+  } 
+  else if (buttonState == HIGH && count == 1) {
+    count == 0;
+  } 
+  else if (count == 1){
+    const char *msg = "Hello World!!";
     driver.send((uint8_t *)msg, strlen(msg));
     driver.waitPacketSent();
-    delay(1000);
+    delay(1250);
+  }
+  else if(count == 0){
+    const char *msg = "new world!!";
+    driver.send((uint8_t *)msg, strlen(msg));
+    driver.waitPacketSent();
+    delay(1250);
+  }
+ 
 }
