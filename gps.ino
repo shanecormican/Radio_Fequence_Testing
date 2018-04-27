133b2e5c7994783c2aa1d59583342efcfd0d8285
shanecormican <32567221+shanecormican@users.noreply.github.com>
2018-04-27 15:40:24 +0100

    Add files via upload

--- gps.ino
+++ gps.ino
@@ -1,23 +1,23 @@
 #include <Adafruit_GPS.h>
 #include <SoftwareSerial.h>
-SoftwareSerial MySerial(5, 2);
+SoftwareSerial MySerial(5, 4);
 Adafruit_GPS GPS(&MySerial);
+
+#include <GSM.h>
+#define PINNUMBER ""
+GSM gsmAccess;
+GSM_SMS sms;
 
 uint8_t c;
 String derection;
 float oldLat = 0;
 float oldLong = 0;
 
+char remoteNumber[20] = "+353851262620";
+
 void setup() {
-  Serial.begin(115200);
-  GPS.begin(9600);
-  GPS.sendCommand("$PGCMD,33,0*6D");
-  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);
-  GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
-  delay(1000);
-  clearGPS();
-  oldLat = GPS.latitudeDegrees;
-  oldLong = GPS.longitudeDegrees;
+  gps_Setup();
+  gsm_Setup();
 }
 
 void loop() {
@@ -57,6 +57,7 @@ void derectionGPS() {
   {
     derection = "still";
   }
+  sendSMS();
   Serial.println(derection);
 }
 
@@ -85,3 +86,40 @@ void printTestCode() {
   Serial.println(GPS.longitudeDegrees, 8);
 }
 
+void sendSMS() {
+
+  // send the message
+  sms.beginSMS(remoteNumber);
+  sms.print(derection);
+  sms.endSMS();
+  Serial.println("\nCOMPLETE!\n");
+}
+
+void gps_Setup() {
+  Serial.begin(115200);
+  GPS.begin(9600);
+  GPS.sendCommand("$PGCMD,33,0*6D");
+  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);
+  GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
+  delay(1000);
+  clearGPS();
+  oldLat = GPS.latitudeDegrees;
+  oldLong = GPS.longitudeDegrees;
+}
+
+void gsm_Setup() {
+  boolean notConnected = true;
+
+  // Start GSM shield
+  // If your SIM has PIN, pass it as a parameter of begin() in quotes
+  while (notConnected)
+  {
+    if (gsmAccess.begin(PINNUMBER) == GSM_READY)
+      notConnected = false;
+    else
+    {
+      Serial.println("Not connected");
+      delay(1000);
+    }
+  }
+}
