e41b55ee25bb6afdb7055e9ab3647ba0ddd967b3
shanecormican <32567221+shanecormican@users.noreply.github.com>
2018-04-30 19:44:40 +0100

    Add files via upload

--- gps_gsm.ino
+++ gps_gsm.ino
@@ -11,89 +11,28 @@ TinyGPSPlus GPS; // The TinyGPS++ object for interfacing with the GPS
 AltSoftSerial ss; // The serial connection object to the GPS device
 
 String derection;
-float oldLat = 53.27892;
-float oldLong = -9.00985;
+//float oldLat = 53.27892;
+//float oldLong = -9.00985;
+float oldLat = 0;
+float oldLong = 0;
 float error_factor = 0.00002;
+
+const int ledPin =  12;
 
 GSM gsmAccess; // Initialise the library instances
 GSM_SMS sms;
 
-char senderNumber[20] = "+353851262620"; // Array to hold the number a SMS is retreived from
+char phoneNumber[20] = "+353851262620";
+char phoneNumber1[20] = "+353851262620";
 
 void setup() {
   vw_set_ptt_inverted(true); // Required for DR3100
-  vw_set_rx_pin(7);
+  vw_set_rx_pin(50);
   vw_setup(4000); // Bits per sec
-  pinMode(13, OUTPUT);
+  pinMode(ledPin, OUTPUT);
 
   vw_rx_start(); // Start the receiver PLL running
-  setupGsm_Gps(); // setup function
-}
 
-void loop() {
-  loopFunction(); // contains all the functions
-  uint8_t buf[VW_MAX_MESSAGE_LEN];
-  uint8_t buflen = VW_MAX_MESSAGE_LEN;
-
-  if (vw_get_message(buf, &buflen)) // Non-blocking
-  {
-    if (buf[0] == '1') {
-      digitalWrite(13, 1);
-    }
-    if (buf[0] == '0') {
-      digitalWrite(13, 0);
-    }
-
-  }
-}
-
-void loopFunction() {
-
-  while (ss.available() > 0) //while there is stuff in the buffer
-    if (GPS.encode(ss.read()) ) //if it can successfully decode it, do it. Else try again when more charachters are in the buffer
-
-      derectionloops(); // chech derection of travel
-  debugingPrint();  // debugging to print derection, latatude, longatude
-  //sendText(); // code to send the text message
-
-  delay(10000); // delay
-}
-
-void derectionloops() {
-  if (GPS.location.lng() < oldLong - error_factor && GPS.location.lat() == oldLat) {
-    derection = "East";
-  }
-  else if (GPS.location.lng() > oldLong + error_factor && GPS.location.lat() == oldLat) {
-    derection = "West";
-  }
-  else  if (GPS.location.lat() < oldLat - error_factor && GPS.location.lng() == oldLong) {
-    derection = "South";
-  }
-  else if (GPS.location.lat() > oldLat + error_factor && GPS.location.lng() == oldLong) {
-    derection = "North";
-  }
-  else if (GPS.location.lat() == 0.0 && GPS.location.lng() == 0.0 ) {
-    derection = "calibrating";
-  }
-  else if (GPS.location.lat() > oldLat + error_factor && GPS.location.lng() > oldLong + error_factor) {
-    derection = "NorthEast";
-  }
-  else if (GPS.location.lat() > oldLat + error_factor && GPS.location.lng() < oldLong - error_factor) {
-    derection = "SouthWest";
-  }
-  else if (GPS.location.lat() < oldLat - error_factor && GPS.location.lng() < oldLong - error_factor) {
-    derection = "NorthWest";
-  }
-  else if (GPS.location.lat() < oldLat - error_factor && GPS.location.lng() > oldLong + error_factor ) {
-    derection = "SouthEast";
-  }
-  else
-  {
-    derection = "still";
-  }
-}
-
-void setupGsm_Gps() {
   ss.begin(GPSBaud); // begin the GPS serial connection
 
   Serial.begin(9600); // begin Serial communication with the computer at 9600 baud rate
@@ -104,31 +43,87 @@ void setupGsm_Gps() {
 
   while (notConnected) // until it connects
   {
-    if (gsmAccess.begin(PINNUMBER) == GSM_READY) // if it succeeds connecting
+    Serial.println("0");
+    if (gsmAccess.begin(PINNUMBER) == GSM_READY) { // if it succeeds connecting
       notConnected = false; // connected
+    }
     else
     {
       Serial.println("Not connected"); // print to the computer
       delay(1000); //delay
     }
   }
+  Serial.println("4");
 }
 
-void sendText() {
-  if ( derection != "still" || derection != "calibrating") {
-    sms.beginSMS(senderNumber); // begin an sms to the sender number
-    sms.print("Alert Bull Escaped!!"); // append a comma
-    sms.print("Heading:" + derection); // append a comma
-    sms.endSMS(); //send the sms
+void loop() {
+
+  uint8_t buf[VW_MAX_MESSAGE_LEN];
+  uint8_t buflen = VW_MAX_MESSAGE_LEN;
+
+  if (vw_get_message(buf, &buflen)) // Non-blocking
+  {
+    if (buf[0] == '1') {
+      digitalWrite(ledPin, 1);
+      Serial.println("Panic Button !!!");
+      sms.beginSMS(phoneNumber1); // begin an sms to the sender number
+      sms.print("John Smith Farm Panic Button has been pressed!!! "); // append a comma
+      sms.print("\nPlease check if John Smith is ok. "); // append a comma
+        sms.print("\nBE WARE OF BULL!!! "); // append a comma
+      sms.endSMS(); //send the sms
+    }
+    if (buf[0] == '0') {
+      digitalWrite(ledPin, 0);
+      while (ss.available() > 0) //while there is stuff in the buffer
+        if (GPS.encode(ss.read()) ) //if it can successfully decode it, do it. Else try again when more charachters are in the buffer
+
+
+          if (GPS.location.lng() < oldLong - error_factor && GPS.location.lat() == oldLat) {
+            derection = "East";
+          }
+          else if (GPS.location.lng() > oldLong + error_factor && GPS.location.lat() == oldLat) {
+            derection = "West";
+          }
+          else  if (GPS.location.lat() < oldLat - error_factor && GPS.location.lng() == oldLong) {
+            derection = "South";
+          }
+          else if (GPS.location.lat() > oldLat + error_factor && GPS.location.lng() == oldLong) {
+            derection = "North";
+          }
+          else if (GPS.location.lat() == 0.0 && GPS.location.lng() == 0.0 ) {
+            derection = "calibrating";
+          }
+          else if (GPS.location.lat() > oldLat + error_factor && GPS.location.lng() > oldLong + error_factor) {
+            derection = "NorthEast";
+          }
+          else if (GPS.location.lat() > oldLat + error_factor && GPS.location.lng() < oldLong - error_factor) {
+            derection = "SouthWest";
+          }
+          else if (GPS.location.lat() < oldLat - error_factor && GPS.location.lng() < oldLong - error_factor) {
+            derection = "NorthWest";
+          }
+          else if (GPS.location.lat() < oldLat - error_factor && GPS.location.lng() > oldLong + error_factor ) {
+            derection = "SouthEast";
+          }
+          else
+          {
+            derection = "still";
+          }
+      Serial.print("direction: ");
+      Serial.println(derection);
+      Serial.print("lat: ");
+      Serial.println(GPS.location.lat(), 5);
+      Serial.print("lng: ");
+      Serial.println(GPS.location.lng(), 5);
+
+      //      if ( derection != "still" || derection != "calibrating") {
+      //        sms.beginSMS(phoneNumber); // begin an sms to the sender number
+      //        sms.print("Alert Bull Escaped!!"); // append a comma
+      //        sms.print("Heading:" + derection); // append a comma
+      //        sms.endSMS(); //send the sms
+      //      }
+    }
   }
+  delay(1000);
 
-}
-
-void debugingPrint() {
-  Serial.print("direction: ");
-  Serial.println(derection);
-  Serial.print("lat: ");
-  Serial.println(GPS.location.lat(), 5);
-  Serial.print("lng: ");
-  Serial.println(GPS.location.lng(), 5);
 }
