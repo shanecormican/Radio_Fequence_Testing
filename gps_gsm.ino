75966591c6bd78dbb1f6a4927f280f2e648d5d46
shanecormican <32567221+shanecormican@users.noreply.github.com>
2018-04-30 11:16:56 +0100

    Add files via upload

--- gps_gsm.ino
+++ gps_gsm.ino
@@ -1,8 +1,8 @@
 #include <AltSoftSerial.h> //Include all relevant libraries - see above
 #include <TinyGPS++.h>
 #include <GSM.h>
+#include <VirtualWire.h>
 #define PINNUMBER "" // PIN Number for the SIM - leave blank unless your SIM has a pin, this is inserted between ""
-
 
 static const uint32_t GPSBaud = 9600; //Baud rate for communication with the GPS, Adafruit GPS = 9600, your GPS may well be 4800, check the spec
 
@@ -20,12 +20,31 @@ GSM_SMS sms;
 
 char senderNumber[20] = "+353851262620"; // Array to hold the number a SMS is retreived from
 
-void setup(){
+void setup() {
+  vw_set_ptt_inverted(true); // Required for DR3100
+  vw_set_rx_pin(7);
+  vw_setup(4000); // Bits per sec
+  pinMode(13, OUTPUT);
+
+  vw_rx_start(); // Start the receiver PLL running
   setupGsm_Gps(); // setup function
 }
 
-void loop(){
+void loop() {
   loopFunction(); // contains all the functions
+  uint8_t buf[VW_MAX_MESSAGE_LEN];
+  uint8_t buflen = VW_MAX_MESSAGE_LEN;
+
+  if (vw_get_message(buf, &buflen)) // Non-blocking
+  {
+    if (buf[0] == '1') {
+      digitalWrite(13, 1);
+    }
+    if (buf[0] == '0') {
+      digitalWrite(13, 0);
+    }
+
+  }
 }
 
 void loopFunction() {
@@ -33,9 +52,7 @@ void loopFunction() {
   while (ss.available() > 0) //while there is stuff in the buffer
     if (GPS.encode(ss.read()) ) //if it can successfully decode it, do it. Else try again when more charachters are in the buffer
 
-
-
-  derectionloops(); // chech derection of travel
+      derectionloops(); // chech derection of travel
   debugingPrint();  // debugging to print derection, latatude, longatude
   //sendText(); // code to send the text message
 
@@ -115,4 +132,3 @@ void debugingPrint() {
   Serial.print("lng: ");
   Serial.println(GPS.location.lng(), 5);
 }
-
